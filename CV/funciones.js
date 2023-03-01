97f69f5988547a769a0e4f93b36df9dfd41421ec
Camila1santillan <126616129+Camila1santillan@users.noreply.github.com>
2023-02-28 22:42:46 -0300

    Add files via upload

---
+++ CV/funciones.js
@@ -0,0 +1,23 @@
+document.getElementById('Boton').addEventListener('click', function () {
+    document.getElementById('inter').style.display = 'none'; 
+});
+
+
+
+document.getElementById("Boton").addEventListener("click", function () { document.getElementById("inter").style.display = "none";
+
+});
+let hideText_btn = document.getElementById('hideText_btn');
+let hideText = document.getElementById('hideText');
+hideText_btn.addEventListener('click', toggleText);
+function toggleText() {
+    hideText.classList.toggle('show');
+    if(hideText.classList.contains('show')) {
+        hideText_btn.innerHTML = 'Leer menos'
+    }
+    else{
+        hideText_btn.innerHTML = 'Leer mas'
+        }
+     }
+
+     
